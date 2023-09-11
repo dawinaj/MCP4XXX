85c11f3d5b6262384c8b5c13830586c33aa20188
Dawid Najda <herhor67@interia.pl>
2023-09-11 19:23:34 +0200

    Update MCP3x0x.h

--- main/include/MCP3x0x.h
+++ main/include/MCP3x0x.h
@@ -35,31 +35,36 @@ enum mcp_signed_t : bool
 template <mcp_channels_t C, mcp_bits_t B, mcp_signed_t S = MCP_DATA_UNSIGNED>
 class MCP3x0x
 {
-	spi_device_handle_t spi_hdl;
-	int32_t ref_mvolt;
-	float scale;
-
-	static constexpr uint8_t requ_len = 2 + ((C == MCP_CHANNELS_2) ? 1 : 3) + 2;
-	static constexpr uint8_t totl_len = requ_len + B;
-	static constexpr uint8_t bytes_len = (totl_len - 1) / 8 + 1;
-	static constexpr int32_t resp_mask = (1u << B) - 1;
+	using uout_t = std::conditional_t<(B <= 8), uint8_t, uint16_t>;
+	using sout_t = std::conditional_t<(B <= 8), int8_t, int16_t>;
 
 public:
-	static constexpr int32_t raw_max = S ? (1u << (B - 1)) : (1u << B);
-	static constexpr int32_t raw_min = S ? (1u << (B - 1)) - 1 : 0;
+	using out_t = std::conditional_t<S, sout_t, uout_t>;
 
-	MCP3x0x(spi_host_device_t spihost, gpio_num_t csgpio, int clkhz = 1'000'000, int rmv = 5000, float sc = 1) : ref_mvolt(rmv), scale(sc)
+private:
+	spi_device_handle_t spi_hdl;
+	float ref_volt;
+
+	static constexpr uout_t resp_mask = (1u << B) - 1;
+	static constexpr uout_t sign_mask = S ? (1u << (B - 1)) : 0; // only used for signed anyway, but w/e
+
+public:
+	static constexpr out_t max = S ? resp_mask / 2 : resp_mask;
+	static constexpr out_t min = S ? -1 - max : 0;
+
+	MCP3x0x(spi_host_device_t spihost, gpio_num_t csgpio, int clkhz = 1'000'000, float rv = 5.0f) : ref_volt(rv)
 	{
 		esp_err_t ret = ESP_OK;
 
 		const spi_device_interface_config_t dev_cfg = {
-			.command_bits = 0,
-			.address_bits = 0,
+			.command_bits = 2,
+			.address_bits = (C == MCP_CHANNELS_2) ? 1 : 3,
+			.dummy_bits = 2,
 			.mode = 0,
 			.clock_speed_hz = clkhz,
 			.input_delay_ns = 0,
 			.spics_io_num = csgpio,
-			.flags = SPI_DEVICE_NO_DUMMY,
+			.flags = SPI_DEVICE_HALFDUPLEX,
 			.queue_size = 1,
 			.pre_cb = NULL,
 			.post_cb = NULL,
@@ -69,6 +74,11 @@ public:
 		ESP_GOTO_ON_ERROR(
 			spi_bus_add_device(spihost, &dev_cfg, &spi_hdl),
 			err, TAG, "Failed to add device to SPI bus!");
+
+		// int khz;
+		// spi_device_get_actual_freq(spi_hdl, &khz);
+
+		// ESP_LOGW(TAG, "kHz: %i", khz);
 		return;
 
 	err:
@@ -86,118 +96,126 @@ public:
 	err:
 		ESP_LOGE(TAG, "Failed to destruct MCP! Error: %s", esp_err_to_name(ret));
 	}
+
 	//
 
-	esp_err_t acquire_spi(TickType_t timeout)
+	esp_err_t acquire_spi(TickType_t timeout = portMAX_DELAY)
 	{
 		ESP_RETURN_ON_ERROR(
 			spi_device_acquire_bus(spi_hdl, timeout),
 			TAG, "Failed to acquire SPI bus!");
+
+		// spi_acq = true;
 
 		return ESP_OK;
 	}
 
 	esp_err_t release_spi()
 	{
-		ESP_RETURN_ON_ERROR(
-			spi_device_release_bus(spi_hdl),
-			TAG, "Failed to acquire SPI bus!");
-
+		spi_device_release_bus(spi_hdl); // return void
+		// spi_acq = false;
 		return ESP_OK;
 	}
-
-	int32_t get_signed_raw(uint8_t chnl, mcp_read_mode_t rdmd = MCP_READ_SINGLE, size_t scnt = 1)
-	{
-
-		spi_transaction_t spi_trx = make_transaction(chnl, rdmd);
-
-		int32_t sum = 0;
-		int32_t raw;
-
-		for (size_t i = 0; i < scnt; ++i)
+	/*/
+		out_t get_signed_raw(uint8_t chnl, mcp_read_mode_t rdmd = MCP_READ_SINGLE, size_t scnt = 1)
 		{
-			raw = get_response(&spi_trx);
-			if (raw == -1)
-				goto errgs;
-			sum += raw;
+
+			spi_transaction_t spi_trx = make_transaction(chnl, rdmd);
+
+			int32_t sum = 0;
+			int32_t raw;
+
+			for (size_t i = 0; i < scnt; ++i)
+			{
+				raw = get_response(&spi_trx);
+				if (raw == -1)
+					goto errgs;
+				sum += raw;
+			}
+
+			return (sum + ((sum < 0) ? -scnt : scnt) / 2) / scnt;
+
+		errgs:
+			return 0;
 		}
-
-		return (sum + ((sum < 0) ? -scnt : scnt) / 2) / scnt;
-
-	errgs:
-		return 0;
-	}
+	//*/
 
 	float get_float_volt(uint8_t chnl, mcp_read_mode_t rdmd = MCP_READ_SINGLE, size_t scnt = 1)
 	{
-		spi_transaction_t spi_trx = make_transaction(chnl, rdmd);
+		spi_transaction_t trx1 = make_transaction(chnl, rdmd);
+		spi_transaction_t trx2 = trx1;
 
 		int32_t sum = 0;
-		int32_t raw;
 
-		for (size_t i = 0; i < scnt; ++i)
+		size_t i = 0;
+
+		send_trx(trx1);
+		++i;
+
+		while (i < scnt)
 		{
-			raw = get_response(&spi_trx);
-			if (raw == -1)
-				goto errgs;
-			sum += raw;
+			recv_trx();
+			send_trx(trx2);
+			++i;
+			sum += parse_trx(trx1);
+			recv_trx();
+			if (i < scnt)
+			{
+				send_trx(trx1);
+				++i;
+			}
+			sum += parse_trx(trx2);
 		}
 
-		return sum / 1000.0f / scnt * ref_mvolt / raw_max * scale;
+		return sum * ref_volt / scnt / max;
 
-	errgs:
-		return 0;
+		// errgs:
+		// 	return 0;
 	}
 
 private:
-	int32_t get_response(spi_transaction_t *spi_trx)
+	inline esp_err_t send_trx(spi_transaction_t &trx)
 	{
 		esp_err_t ret = ESP_OK;
 
-		int32_t recv = 0;
-		uint8_t *bytes = reinterpret_cast<uint8_t *>(&recv);
+		ESP_GOTO_ON_ERROR(
+			spi_device_polling_start(spi_hdl, &trx, portMAX_DELAY),
+			err, TAG, "Error in spi_device_polling_start()");
+
+		return ret;
+	err:
+		ESP_LOGE(TAG, "Error in send_trx(): %s", esp_err_to_name(ret));
+		return ret;
+	}
+
+	inline esp_err_t recv_trx()
+	{
+		esp_err_t ret = ESP_OK;
 
 		ESP_GOTO_ON_ERROR(
-			spi_device_transmit(spi_hdl, spi_trx),
-			errgr, TAG, "Error in get_response()");
+			spi_device_polling_end(spi_hdl, portMAX_DELAY),
+			err, TAG, "Error in spi_device_polling_end()");
 
-		switch (bytes_len)
-		{
-		case 4:
-			bytes[3] = spi_trx->rx_data[0];
-			bytes[2] = spi_trx->rx_data[1];
-			bytes[1] = spi_trx->rx_data[2];
-			bytes[0] = spi_trx->rx_data[3];
-			ESP_LOGV(TAG, "SPI 4 bytes!");
-			break;
-		case 3:
-			bytes[2] = spi_trx->rx_data[0];
-			bytes[1] = spi_trx->rx_data[1];
-			bytes[0] = spi_trx->rx_data[2];
-			ESP_LOGV(TAG, "SPI 3 bytes!");
-			break;
-		case 2:
-			bytes[1] = spi_trx->rx_data[0];
-			bytes[0] = spi_trx->rx_data[1];
-			ESP_LOGV(TAG, "SPI 2 bytes!");
-			break;
-		default:
-			bytes[0] = spi_trx->rx_data[0];
-			ESP_LOGV(TAG, "SPI 1 bytes!");
-			break;
-		};
+		return ret;
+	err:
+		ESP_LOGE(TAG, "Error in recv_trx(): %s", esp_err_to_name(ret));
+		return ret;
+	}
 
-		recv &= resp_mask;
+	inline out_t parse_trx(spi_transaction_t &trx)
+	{
+		out_t out;
 
-		if constexpr (S == MCP_DATA_SIGNED) // if signed
-			if (recv >> B - 1)				// if negative
-				recv |= ~resp_mask;
+		if constexpr (B <= 8)
+			out = trx.rx_data[0] >> (8 - B);
+		else
+			out = __builtin_bswap16(*reinterpret_cast<uint16_t *>(trx.rx_data)) >> (16 - B);
 
-		return recv;
+		if constexpr (S)		   // if signed
+			if (out & sign_mask)   // if negative
+				out |= ~resp_mask; // flip high sign bits
 
-	errgr:
-		ESP_LOGE(TAG, "Error: %s", esp_err_to_name(ret));
-		return 0;
+		return out;
 	}
 
 	spi_transaction_t make_transaction(uint8_t chnl, mcp_read_mode_t rdmd)
@@ -224,51 +242,17 @@ private:
 		//     - BY: most significant bit.
 		//     - B0: least significant bit.
 
-		uint32_t send = 5;
-		uint8_t *bytes = reinterpret_cast<uint8_t *>(&send);
+		spi_transaction_t trx;
 
-		chnl &= (C == MCP_CHANNELS_2) ? 0b1 : 0b111;
+		trx.cmd = 0b10 | rdmd; // start bit and read mode
+		trx.addr = chnl;	   // &= (C == MCP_CHANNELS_2) ? 0b1 : 0b111; but wil be truncated anyway
+		trx.flags = SPI_TRANS_USE_RXDATA;
 
-		// start bit
-		send |= 1 << (totl_len - 1);
-		// single/diff
-		send |= rdmd << (totl_len - 2);
-		// channel
-		send |= chnl << (totl_len - requ_len + 2);
+		trx.tx_buffer = nullptr;
+		trx.length = 0;
+		trx.rxlength = B;
 
-		spi_transaction_t spi_trx = {
-			.flags = SPI_TRANS_USE_RXDATA | SPI_TRANS_USE_TXDATA,
-		};
-
-		spi_trx.length = bytes_len * 8;
-
-		switch (bytes_len)
-		{
-		case 4:
-			spi_trx.tx_data[0] = bytes[3];
-			spi_trx.tx_data[1] = bytes[2];
-			spi_trx.tx_data[2] = bytes[1];
-			spi_trx.tx_data[3] = bytes[0];
-			ESP_LOGV(TAG, "SPI 4 bytes!");
-			break;
-		case 3:
-			spi_trx.tx_data[0] = bytes[2];
-			spi_trx.tx_data[1] = bytes[1];
-			spi_trx.tx_data[2] = bytes[0];
-			ESP_LOGV(TAG, "SPI 3 bytes!");
-			break;
-		case 2:
-			spi_trx.tx_data[0] = bytes[1];
-			spi_trx.tx_data[1] = bytes[0];
-			ESP_LOGV(TAG, "SPI 2 bytes!");
-			break;
-		default:
-			spi_trx.tx_data[0] = bytes[0];
-			ESP_LOGV(TAG, "SPI 1 bytes!");
-			break;
-		};
-
-		return spi_trx;
+		return trx;
 	}
 };
 
